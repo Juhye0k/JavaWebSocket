4739db7025a72819c5934be9eaf8511fe7d2b7e1
Juhye0k <kyoung1678@naver.com>
2025-04-02 19:41:10 +0900

    refactor: 응답 객체 수정

--- src/main/java/me/juhye0k/dto/MessagePacketResponse.java
+++ src/main/java/me/juhye0k/dto/MessagePacketResponse.java
@@ -1,15 +1,38 @@
 package me.juhye0k.dto;
 
-public class MessagePacketResponse { // 서버에서 클라이언트로 보내는 응답 패킷
-    String message;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-    public MessagePacketResponse(){
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MessagePacketResponse {
 
-    }
+    private String message; // 정상 메시지일 때
+    private ErrorResponse error; // 에러가 있을 때
+
+    // 기본 생성자
+    public MessagePacketResponse() {}
+
+    // 성공 응답 생성자
     public MessagePacketResponse(String message) {
         this.message = message;
     }
+
+    // 에러 응답 생성자
+    public MessagePacketResponse(ErrorResponse error) {
+        this.error = error;
+    }
+
+    // 메시지 getter
     public String getMessage() {
         return message;
+    }
+
+    // 에러 getter
+    public ErrorResponse getError() {
+        return error;
+    }
+
+    // 에러 여부 판단
+    public boolean isError() {
+        return error != null;
     }
 }
